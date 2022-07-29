d4c14311c4955aaf691b39fad55983882dd7d71a
Jaideep Kular <kularjaideep@gmail.com>
2022-07-29 02:34:46 -0400

    clean up

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ app.use(express.json());
 
 app.use('/game', game);
 
-app.get('/', (req, res) => {
-    res.send('Get request on /');
-})
-
-app.get('/hello', (req, res) => {
-    res.json(({ message: 'come on' }));
-})
-
-app.get('/questions', (req, res) => {
-    res.json({ message: 'hello' })
-})
-
 app.listen(PORT, () => {
     console.log(`listening on Port ${PORT}`);
 })
-
-// Use MongoDB and host the server on Heroku
-// Host FE on netlify
