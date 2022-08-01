e54be6c45a154f2ef8368906a2912be3cf124724
Jaideep Kular <kularjaideep@gmail.com>
2022-08-01 00:21:36 -0400

    Firebase authentication added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const game = require('./routes/game');
+const verifyToken = require('./middleware/authentication');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
+app.use(verifyToken)
 
 app.use('/game', game);
 
